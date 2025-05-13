ed58dae9b41a44740957be95dde4aaaa6fec527a
atmane hann <hannatoumane@gmail.com>
2025-05-13 12:17:25 +0000

    ajout du class module

---
+++ src/model/Module.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Module {
+	public static void main(String[] args) {
+		  // Déclaration et initialisation des variables
+        String developpement = "Java";
+        String reseau = "TCP/IP";
+        String systeme = "Linux";
+        String merise = "Modélisation UML";
+
+
+	}
+
+}
